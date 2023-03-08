8e2498c383cf6d4b066ffed8bd67d8339aff2c33
Kanha Choubey <kanhaarmyoo7@gmail.com>
2023-03-08 22:56:47 +0530

    some changes

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -6,14 +6,16 @@ const Sales = ({ endpoint }) => {
   const { title, items } = endpoint;
 
   return (
-    <div>
-      <Titles title={title} />
+    <>
       <div>
-        {items?.map((item, i) => (
-          <Item {...item} key={i} />
-        ))}
+        <Titles title={title} />
+        <div className="grid items-center justify-items-center grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-7 lg:gap-5 mt">
+          {items?.map((item, i) => (
+            <Item {...item} key={i} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
